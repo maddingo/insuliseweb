c370f5ece742c5ced8962af53f766bbc43fc457b
Martin Goldhahn <mgoldhahn@gmail.com>
2016-06-15 23:49:37 +0200

    Close application context

--- src/main/java/insuliseweb/Application.java
+++ src/main/java/insuliseweb/Application.java
@@ -2,12 +2,19 @@ package insuliseweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext ctx = null;
+        try {
+            ctx = SpringApplication.run(Application.class, args);
+        } finally {
+            if (ctx != null) {
+                ctx.close();
+            }
+        }
     }
 }
